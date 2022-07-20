081edee6432b0d6fb188bc17013497b04a0a68dc
unknown <katammaniteja@gmail.com>
2022-07-20 18:27:16 +0530

    Setting up firebase to the project

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyANkvZMkDcLGgKyG8hZyYng5PjuyWMYTpU",
+    authDomain: "ytclone-maniteja.firebaseapp.com",
+    projectId: "ytclone-maniteja",
+    storageBucket: "ytclone-maniteja.appspot.com",
+    messagingSenderId: "202256526205",
+    appId: "1:202256526205:web:963301fb701c024bfefa14"
+};
+
+firebase.initializeApp(firebaseConfig);
